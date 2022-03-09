15c05c4943898d7ee7fd02ce87228de22651e7a5
sakikitaori <98959152+sakikitaori@users.noreply.github.com>
2022-03-09 10:18:08 +0900

    [finish]2

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+
+
+  has_many :books
+  has_one_attached :profile_image
+
+
+
+
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+end
