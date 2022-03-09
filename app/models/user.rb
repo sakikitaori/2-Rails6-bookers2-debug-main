f6608a7ef57c9fa739a5275a4655abb756afc50a
sakikitaori <98959152+sakikitaori@users.noreply.github.com>
2022-03-09 17:43:10 +0900

    [finish]2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-
+  validates :introduction, length: {maximum: 50}
 
   has_many :books
   has_one_attached :profile_image
